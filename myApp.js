91f25bd95bc84b35c877c4553ca8898be10a9996
Glitch (smiling-file) <none>
2019-12-14 18:30:45 +0000

    🏚🎿 Updated with Glitch

--- myApp.js
+++ myApp.js
@@ -11,15 +11,15 @@ app.get('[project_url]/api/timestamp/:date_string?', (req, res) => {
   if (dateString.length == 0){
       //Empty string 
       let newDate = new Date()
-      let unixDate = newDate.getTime() * 1           
-      let utcDate =  newDate.toUTCString() ;       
-      res.json({"unix": unixDate, "utc": utcDate}) 
+      //let unixDate = newDate.getTime() * 1           
+      //let utcDate =  newDate.toUTCString() ;       
+      res.json({"unix": newDate.getTime(), "utc": newDate.toUTCString()}) 
   }
   
   if (new Date(dateString)){ 
-        let unixDate = req.query.date_string.getTime() 
-        let utcDate = req.query.date_string.toUTCString()
-        res.json({"unix": unixDate, "utc": utcDate})      
+        //let unixDate = req.query.date_string.getTime() * 1    
+        //let utcDate = req.query.date_string.toUTCString()
+        res.json({"unix": dateString.getTime() , "utc": dateString.toUTCString()})      
   }else{
         res.json({"error" : "Invalid Date" })  
   }    
